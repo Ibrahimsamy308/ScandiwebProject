bd9b54038a2094b810aba1d7960cf2a3ff88ba9c
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2023-03-15 13:28:47 +0200

    Myfirst upload

---
+++ Productlist.php
@@ -0,0 +1,76 @@
+
+    <?php
+
+        //parent class
+      abstract class productt{
+
+            private $sku;
+            private $bname;
+            private $bprice;
+            private $bweight;
+
+
+
+      }
+
+        //child class
+        // class product extends productt{
+
+        //     private $sku;
+        //     private $bname;
+        //     private $bprice;
+        //     private $bweight;
+
+
+        //     public function __construct($sku,$bname,$bprice,$bweight)
+        //     {
+        //         $this->$sku=$sku;
+        //         $this->$bname=$bname;
+        //         $this->$bprice=$bprice;
+        //         $this->$bweight=$bweight;
+        //     }
+
+        //     public function getsku(){
+        //         return $this->sku;
+        //     }
+
+        //     public function getbname(){
+        //         return $this->bname;
+        //     }
+
+        //     public function getbprice(){
+        //         return $this->bprice;
+        //     }
+
+        //     public function getbweight(){
+        //         return $this->bweight;
+        //     }
+
+        //     public function settersku($sku){
+        //         $this->sku=$sku;
+        //     }
+
+        //     public function setterbname($bname){
+        //         $this->bname=$bname;
+        //     }
+
+        //     public function setterbprice($bprice){
+        //         $this->bprice=$bprice;
+        //     }
+
+        //     public function setterbwight($bweight){
+        //         $this->bweight=$bweight;
+        //     }
+
+        // }
+    ?>
+
+
+
+
+
+
+
+
+
+
